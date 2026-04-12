270f92a4cbce249b949cebb02fbbc10f995ed7f7
ansh-m14x <ansh981786@gmail.com>
2026-04-12 20:28:22 +0530

    Update rules.mk

--- dev/rules.mk
+++ dev/rules.mk
@@ -1,13 +1,15 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
+# Essential hardware modules required for A31 initialization
 MODULES += \
 	$(LOCAL_DIR)/video \
 	$(LOCAL_DIR)/lcm \
 	$(LOCAL_DIR)/gic \
 	$(LOCAL_DIR)/md_com
 
-
 OBJS += \
 	$(LOCAL_DIR)/dev.o
 
-include $(LOCAL_DIR)/logo/rules.mk
+# Commented out to fix "No rule to make target" error
+# By removing this, the build system will not look for fhd_uboot.raw
+# include $(LOCAL_DIR)/logo/rules.mk

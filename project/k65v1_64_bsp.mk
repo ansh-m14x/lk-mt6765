9b8975a655ba1e4c9a179e8602c0e04cab2c9887
ansh-m14x <ansh981786@gmail.com>
2026-04-12 20:18:36 +0530

    Update k65v1_64_bsp.mk

--- project/k65v1_64_bsp.mk
+++ project/k65v1_64_bsp.mk
@@ -1,8 +1,17 @@
-#
 LOCAL_DIR := $(GET_LOCAL_DIR)
 TARGET := k65v1_64_bsp
-MODULES += app/mt_boot \
-           dev/lcm
+
+# --- UPDATED FOR OPPO A31 ---
+# 1. Disable Logo Support to stop the "No rule to make target" error
+MTK_LOGO_SUPPORT := no
+BOOT_LOGO := none
+
+# 2. Disable Verified Boot to prevent bootloops on unsigned custom builds
+MTK_SECURITY_SW_SUPPORT := no
+MTK_VERIFIED_BOOT_SUPPORT := no
+MTK_SEC_FASTBOOT_UNLOCK_SUPPORT := yes
+
+# 3. Base Hardware Support
 MTK_EMMC_SUPPORT = yes
 MTK_MMC_COMBO_DRV = yes
 MTK_EMMC_SUPPORT_OTP = yes
@@ -10,36 +19,30 @@ MTK_SMI_SUPPORT = yes
 MTK_KERNEL_POWER_OFF_CHARGING = yes
 DEFINES += MTK_NEW_COMBO_EMMC_SUPPORT
 DEFINES += MTK_GPT_SCHEME_SUPPORT
+
+# 4. Charger & PMU
 MTK_CHARGER_NEW_ARCH := yes
-MTK_PUMP_EXPRESS_PLUS_SUPPORT := yes
-MTK_MT6370_PMU_CHARGER_SUPPORT := yes
-MTK_MT6370_PMU_BLED_SUPPORT := yes
 MTK_CHARGER_INTERFACE := yes
+# A31 uses MediaTek PMIC; setting this ensures the power rails initialize
+DEFINES += MTK_MT6358_PMU 
+
+# 5. Display (LCM)
+# The generic driver (nt35695) is likely wrong. 
+# Keep it empty for now to test if the bootloader initializes.
+CUSTOM_LK_LCM="" 
 MTK_LCM_PHYSICAL_ROTATION = 0
-CUSTOM_LK_LCM="nt35695_fhd_dsi_vdo_truly_rt5081 nt35695B_fhd_dsi_vdo_auo_rt5081"
-DEFINES += MTK_ROUND_CORNER_SUPPORT
-#nt35595_fhd_dsi_cmd_truly_nt50358 = yes
-#FASTBOOT_USE_G_ORIGINAL_PROTOCOL = yes
-MTK_SECURITY_SW_SUPPORT = yes
-MTK_VERIFIED_BOOT_SUPPORT = yes
-MTK_SEC_FASTBOOT_UNLOCK_SUPPORT = yes
-SPM_FW_USE_PARTITION = yes
-BOOT_LOGO := fhd
+
+# 6. Debugging (Use UART for error logs)
 DEBUG := 2
-#DEFINES += WITH_DEBUG_DCC=1
 DEFINES += WITH_DEBUG_UART=1
-#DEFINES += WITH_DEBUG_FBCON=1
-CUSTOM_LK_USB_UNIQUE_SERIAL=no
+
+# 7. System Stability
 MTK_TINYSYS_SCP_SUPPORT = yes
 MTK_TINYSYS_SSPM_SUPPORT = yes
-#DEFINES += NO_POWER_OFF=y
-#DEFINES += FPGA_BOOT_FROM_LK=y
+MTK_LK_ENABLE_DCM = yes
+MTK_MT22_MODE :=H1
 MTK_PROTOCOL1_RAT_CONFIG = C/Lf/Lt/W/T/G
 MTK_GOOGLE_TRUSTY_SUPPORT = no
-DEFINES += MTK_MT6370_PMU
 MTK_EFUSE_WRITER_SUPPORT = no
 MTK_SMC_ID_MGMT = yes
 DEVELOP_STAGE = SB
-MTK_LK_ENABLE_DCM = yes
-MTK_MT22_MODE :=H1
-
